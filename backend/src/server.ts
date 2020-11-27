629e8168be49a123a81f57463e6b626a4ff38ce9
Afranio <afzsilva@gmail.com>
2020-11-27 15:56:52 -0300

    config conxeao com sever.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import app from "./app";
+import database from './database';
+
 const port = 3000;
 
-app.listen(port);
+database.sync({ force: true });
 
+
+
+app.listen(port);
 console.log(`Aplicação NODE e TS rodando da porta ${port}`);
