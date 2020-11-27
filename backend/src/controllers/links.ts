bacb461e27617665e11c63cedcab2ae422bd37c6
Afranio <afzsilva@gmail.com>
2020-11-27 19:56:11 -0300

    refactor controller link

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,10 +1,11 @@
 //CONTROLLER
 import { Request, Response } from "express";
 import { Link } from "../models/link";
+import linkRepository from '../models/linksRepository';
 
 //Estrutura para armazenar dados em memoria
-const links: Link[] = [];
-let proxId = 1;
+//const links: Link[] = [];
+//let proxId = 1;
 
 function generateCode() {
     let text = '';
@@ -17,24 +18,26 @@ function generateCode() {
 }
 
 
-
-function postLink(req: Request, res: Response) {
+async function postLink(req: Request, res: Response) {
 
     const link = req.body as Link;
-    link.id = proxId++;
     link.code = generateCode();
     link.hits = 0;
-    links.push(link);
-    console.log(links);
-    res.status(201).json(link);
+    const result = await linkRepository.add(link);
+    if (!result.id) return res.sendStatus(400);
 
+    link.id = result.id!;
+
+    res.status(201).json(link);
 }
 
 
-function getLink(req: Request, res: Response) {
-    /**Apenas mostra sem contar */
+async function getLink(req: Request, res: Response) {
+    /**Apenas mostra sem contar*/
+
     const code = req.params.code as string;
-    const link = links.find(item => item.code === code);
+    //const link = links.find(item => item.code === code);
+    const link = await linkRepository.findByCode(code);
 
     if (!link) {
         res.sendStatus(404);
@@ -44,20 +47,17 @@ function getLink(req: Request, res: Response) {
 }
 
 
-function hitLink(req: Request, res: Response) {
+async function hitLink(req: Request, res: Response) {
     /**conta os acesso */
     const code = req.params.code as string;
-    const index = links.findIndex(item => item.code === code);
-
-    if (index === -1) {
+    //const index = links.findIndex(item => item.code === code);
+    const link = await linkRepository.hit(code);
+    if (!link) {
         res.sendStatus(404);
     } else {
-        links[index].hits!++;
-
-        res.json(links[index]);
+        res.json(link);
     }
 }
-
 
 export default {
     postLink,
