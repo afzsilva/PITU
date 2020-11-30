61f9d9310ea595fb404b90417aa0185347a8d34a
Afranio <afzsilva@gmail.com>
2020-11-29 22:06:55 -0300

    Ajustes do final

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,7 @@
 //CONEXÃO COM BANCO DE DADOS
 import { Sequelize } from "sequelize";
 
-const connection = new Sequelize('pitu', 'root', '', { host: 'localhost', dialect: 'mysql' });
+//const connection = new Sequelize('pitu', 'root', '', { host: 'localhost', dialect: 'mysql' });
+const connection = new Sequelize('mysql://root:mysql20pitu_ML@localhost:3306/pitu');
 
 export default connection;
